5efd802ae840aa447eed8036bdd1ba1fb2fbe031
Гаджиахмед Рамазанов <gadjiram@icloud.com>
2021-10-21 08:52:51 +0300

    добавлена функция IsLeapYear, проверяет високосность года

--- DateTime.pas
+++ DateTime.pas
@@ -1,3 +1,9 @@
 ﻿begin
-  
+  function IsLeapYear(year:integer):boolean;
+  begin
+    if (year mod 4 = 0) and ((year mod 100 <> 0) or (year mod 400 = 0)) then begin
+      result:=true;
+    end
+    else result:=false;
+  end;
 end.
